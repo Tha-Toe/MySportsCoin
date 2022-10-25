aa46b10e614317258c8bbecf0e8f955dce43ccbf
tha toe saung <thatoe510@gmail.com>
2022-10-25 22:56:47 +0630

    add loading

--- src/component/data/Data.jsx
+++ src/component/data/Data.jsx
@@ -3,10 +3,13 @@ import "./data.css";
 import { collection, onSnapshot, query } from "firebase/firestore";
 import { db } from "../../firebase";
 import { useState } from "react";
+import LoadingSpinner from "../LoadingSpinner";
 export default function Data() {
   const [dataFromFirebase, setDataFromFirebase] = useState([]);
+  const [showSpinner, setShowSpinner] = useState(false);
   useEffect(() => {
     const getData = async () => {
+      setShowSpinner(true);
       const q = query(collection(db, "data"));
       onSnapshot(q, (querySnapshot) => {
         const cities = [];
@@ -14,7 +17,7 @@ export default function Data() {
           cities.push(doc.data());
         });
         setDataFromFirebase(cities);
-        console.log(cities);
+        setShowSpinner(false);
       });
     };
     getData();
@@ -22,17 +25,21 @@ export default function Data() {
   return (
     <div className="data_container">
       <div className="data_header">Data</div>
-      <div className="data_child">
-        {dataFromFirebase.map((e, index) => (
-          <div className="data_each_container" key={index}>
-            <div>{index + 1}.</div>
-            <div className="each_data">First Name: {e.firstName}</div>
-            <div className="each_data">Second Name: {e.secondName}</div>
-            <div className="each_data">Email: {e.email}</div>
-            <div className="each_data"> phone number: {e.phoneNumber}</div>
-          </div>
-        ))}
-      </div>
+      {showSpinner ? (
+        <LoadingSpinner />
+      ) : (
+        <div className="data_child">
+          {dataFromFirebase.map((e, index) => (
+            <div className="data_each_container" key={index}>
+              <div>{index + 1}.</div>
+              <div className="each_data">First Name: {e.firstName}</div>
+              <div className="each_data">Second Name: {e.secondName}</div>
+              <div className="each_data">Email: {e.email}</div>
+              <div className="each_data"> phone number: {e.phoneNumber}</div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
