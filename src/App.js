f631aac6c031d54073843c5a8d2d12d092156f50
tha toe saung <thatoe510@gmail.com>
2022-10-25 21:58:03 +0630

    add firebase data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Home from "./component/Home";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Data from "./component/data/Data";
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/data" element={<Data />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
